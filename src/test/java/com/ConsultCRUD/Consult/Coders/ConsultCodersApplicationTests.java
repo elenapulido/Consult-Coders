8bdf9ae728b4c0085cf9fc201eee99490b713a1e
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-07 13:14:21 +0100

    Initial commit

---
+++ src/test/java/com/ConsultCRUD/Consult/Coders/ConsultCodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConsultCRUD.Consult.Coders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultCodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
