e085b10f361e106f42c204cd07287b723ea13b74
Jeann11 <jeanneth8464@gmail.com>
2023-03-08 15:24:55 +0100

    change

---
+++ src/main/java/com/ConsultCRUD/Consult/Coders/CConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.ConsultCRUD.Consult.Coders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CConsultationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CConsultationApplication.class, args);
+    }
+
+}
+
