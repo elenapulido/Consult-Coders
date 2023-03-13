fb21c734bc81988dbcd39632a3090f1d6e4af268
Jeann11 <jeanneth8464@gmail.com>
2023-03-13 20:55:38 +0100

    add view

---
+++ src/main/java/com/ConsultCRUD/Consult/Coders/models/Coder.java
@@ -0,0 +1,15 @@
+package com.ConsultCRUD.Consult.Coders.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "coders")
+public class Coder {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id_coder")
+    private int id;
+    private String name;
+    private String surname;
+    private String age;
+
+}
