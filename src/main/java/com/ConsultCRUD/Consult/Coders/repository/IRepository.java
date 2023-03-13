7cebac7b293ba0c647a09ff75635a384000b4add
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-13 18:39:47 +0100

    IRepository

--- src/main/java/com/ConsultCRUD/Consult/Coders/repository/IRepository.java
+++ src/main/java/com/ConsultCRUD/Consult/Coders/repository/IRepository.java
@@ -1,7 +1,10 @@
 package com.ConsultCRUD.Consult.Coders.repository;
 
+import com.ConsultCRUD.Consult.Coders.Model.CCoder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface IRepository extends JpaRepository {
+@Repository
+public interface IRepository extends JpaRepository <CCoder, Long> {
 
 }
