9f86d376798c55305d87ebc9e6a3ec636cb00681
JavierBP <fjavierbresperez@yahoo.es>
2023-03-17 09:13:57 +0100

    update *.html

--- src/main/java/com/ConsultCRUD/Consult/Coders/Model/CForm.java
+++ src/main/java/com/ConsultCRUD/Consult/Coders/Model/CForm.java
@@ -1,10 +1,7 @@
 package com.ConsultCRUD.Consult.Coders.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,15 +14,11 @@ public class CForm {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
     private String lastname;
     private String phone;
-
     private String email;
     private String theme;
-
-
-
 
 }
--- src/main/java/com/ConsultCRUD/Consult/Coders/controllers/HomeController.java
+++ src/main/java/com/ConsultCRUD/Consult/Coders/controllers/HomeController.java
@@ -4,28 +4,36 @@ import com.ConsultCRUD.Consult.Coders.Model.CForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping
 public class HomeController {
 
-     @GetMapping("")
+
+    @GetMapping("/home")
     public String home() {
-        return "index";
+
+        return "home";
     }
 
     @GetMapping("/form")
     public String FormController(Model model) {
-        model.addAttribute("form", new CForm());
+        model.addAttribute("cform", new CForm());
         return "form";
     }
 
     @GetMapping("/list")
     public String list() {
+
         return "list";
     }
 
     @GetMapping("/edit")
     public String edit() {
+
         return "edit";
     }
 
