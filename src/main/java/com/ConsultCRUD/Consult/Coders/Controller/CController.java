aa61e0de81cc9a24300478c189a3a5e88bb187dd
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-17 13:26:24 +0100

    readConsultation

--- src/main/java/com/ConsultCRUD/Consult/Coders/Controller/CController.java
+++ src/main/java/com/ConsultCRUD/Consult/Coders/Controller/CController.java
@@ -1,5 +1,6 @@
 package com.ConsultCRUD.Consult.Coders.Controller;
 
+import org.springframework.ui.Model;
 import com.ConsultCRUD.Consult.Coders.Model.CConsultation;
 import com.ConsultCRUD.Consult.Coders.Service.CConsultationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,36 +15,44 @@ public class CController {
     @Autowired
     CConsultationService cConsultationService;
 
-    @GetMapping("/consultation")
-   public String List (Model model) readConsultation(){
-        return cConsultationService.readConsultation();
+    @GetMapping("/")
+   public String readConsultation(Model model){
+        model.addAttribute("consultation",cConsultationService.readConsultation() );
+        return "home";
 
     }
-    @GetMapping("/read/{id}")
-    private Optional<CConsultation> readConsultationId(@PathVariable("id") Long id){
-       return cConsultationService.readConsultationId(id);
+
+    @GetMapping("/edit/{id}")
+    public String readConsultationId(@PathVariable("id") Long id, Model model){
+        model.addAttribute("consultation",cConsultationService.readConsultationId(id) );
+        return "home";
+
+    }
+    @PostMapping ("/edit/{id}")
+    public String updateConsultation(@RequestBody CConsultation consultation,@PathVariable("id")Long id){
+        cConsultationService.updateConsultation(consultation,id);
+        return "list";
 
     }
 
     @PostMapping("/create")
     public String createConsultation(@RequestBody CConsultation consultation){
         cConsultationService.createConsultation(consultation);
+        return "redirect: /";
 
     }
-    @PostMapping ("/edit/{id}")
-    public String updateConsultation(@RequestBody CConsultation consultation,@PathVariable("id")Long id){
-        cConsultationService.updateConsultation(consultation,id);
+    @GetMapping("/create")
+    public String createConsultationGet(@RequestBody CConsultation consultation){
+        cConsultationService.createConsultation(consultation);
+        return "form";
 
     }
-
 
     @GetMapping ("/delete/{id}")
     public String deleteConsultation(@PathVariable("id")Long id){
         cConsultationService.deleteConsultation(id);
-
+        return "list";
 
     }
-
-
 
 }
