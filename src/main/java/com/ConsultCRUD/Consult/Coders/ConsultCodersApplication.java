db4f8ace9fee63911410d83d7177d1fea92b5ba8
Jeann11 <jeanneth8464@gmail.com>
2023-03-17 12:30:05 +0100

    change3

--- src/main/java/com/ConsultCRUD/Consult/Coders/ConsultCodersApplication.java
+++ src/main/java/com/ConsultCRUD/Consult/Coders/ConsultCodersApplication.java
@@ -10,14 +10,5 @@ public class ConsultCodersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultCodersApplication.class, args);
 	}
-<<<<<<< HEAD
 
-=======
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	
-		return String.format("Hello Ibias and everybody!", name);
-
-	}
->>>>>>> 26b84b9158a027027ca2bbe9470b34599e3e57e1
 }
