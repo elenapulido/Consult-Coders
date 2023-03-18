0dd248431aa285b653039f87c45f6769bbb2ede8
Jeann11 <jeanneth8464@gmail.com>
2023-03-18 18:38:50 +0100

    readme

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO coders (id,name,surname,email,phone,theme,date) VALUES (default,'Pepe','Perez','root@mail.com',600123123,'JAVA','18/01/1999');
-INSERT INTO coders (id,name,surname,email,phone,theme,date) VALUES (default,'María','Perez','root@mail.com',600123124,'JAVA','18/01/2000');
+INSERT INTO coders (id,name,surname,email,phone,theme,date) VALUES (default,'Pepe','Perez','pepe@email.com',600123123,'JAVA','17/03/2023');
+INSERT INTO coders (id,name,surname,email,phone,theme,date) VALUES (default,'María','Herrera','maria@email.com',600123124,'HTML','18/03/2023');
+INSERT INTO coders (id,name,surname,email,phone,theme,date) VALUES (default,'Eloisa','Jerez','eloisa@email.com',600123125,'CSS','19/03/2023');
