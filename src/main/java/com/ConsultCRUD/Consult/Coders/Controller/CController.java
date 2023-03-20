6d996058c806c685169e0b096506b83e3f271f53
JavierBP <fjavierbresperez@yahoo.es>
2023-03-20 10:19:32 +0100

    little change CController

--- src/main/java/com/ConsultCRUD/Consult/Coders/Controller/CController.java
+++ src/main/java/com/ConsultCRUD/Consult/Coders/Controller/CController.java
@@ -23,7 +23,7 @@ public class CController {
 
     @GetMapping("/list")
     public String readConsultation(Model model){
-        model.addAttribute("consultation",cConsultationService.readConsultation() );
+        model.addAttribute("consultation",cConsultationService.readConsultationId() );
         return "list";
 
     }
